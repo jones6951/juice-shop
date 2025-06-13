48b0b80e39c28e1d3d6aecbdeb83be85df00eeea
dc-moses <Dylan.c.moses@gmail.com>
2025-06-13 15:24:26 -0400

    updating instance type

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "aws_subnet" "juice_subnet" {
   vpc_id                  = aws_vpc.juice_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"  # Explicitly set the availability zone
 }
 
 resource "aws_internet_gateway" "gw" {
